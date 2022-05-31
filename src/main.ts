38bf3395acc9df5565dc8eeddc05eacccb582220
dagbouj hatem <dagboujhatem@gmail.com>
2022-05-31 19:12:59 +0100

    install swagger & create todo resource

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+    .setTitle('Swagger APIs documentation')
+    .setDescription('The todos API description')
+    .setVersion('1.0')
+    .addTag('nestsjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
